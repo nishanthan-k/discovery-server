0d61d5ee938e448ba4def43b3fa291f1813c1fa8
Nishanthan <krn.nishanthan@gmail.com>
2025-02-21 17:42:57 +0530

    feat: vercel setup

--- dist/routes/register.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const register_controller_1 = require("../controllers/register.controller");
-const router = (0, express_1.Router)();
-router.get('/', register_controller_1.registerService);
-exports.default = router;
--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,20 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use('/api/registers', registerRoutes);
 
-app.use('/api/registers', registerRoutes)
+app.get("/", asyncHandler((req: Request, res: Response) => res.send("Hii")));
 
-// app.get("/", asyncHandler((req: Request, res: Response) => res.send("Hii ")));
+// In local development, run the server
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => console.log('Server started on port: ', PORT));
+// For Vercel deployment
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  return app(req, res);
+};
